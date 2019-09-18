c737d880709598c226512f8cffb20e2668c29803
Gerencia Virtual <gerencia.virtual@hotmail.com>
2019-09-18 13:58:18 -0500

    Se agrega controller de documento costo en roaming

---
+++ IC2/Controllers/RoamingDocumentoCostoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IC2.Controllers
+{
+    public class RoamingDocumentoCostoController : Controller
+    {
+        // GET: RoamingDocumentoCosto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CargarCSV(HttpPostedFileBase archivoCSV, int lineaNegocio)
+        {
+            
+            object respuesta = null;
+            
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
