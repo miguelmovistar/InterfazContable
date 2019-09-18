7453dc61b76214a741e31f5728aca3b7c66fabf6
Gerencia Virtual <gerencia.virtual@hotmail.com>
2019-09-18 16:16:12 -0500

    Se crea controller para la carga de cancelacion costo de roaming

---
+++ IC2/Controllers/RoamingCancelacionCostoController.cs
@@ -0,0 +1,29 @@
+﻿using IC2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IC2.Controllers
+{
+    public class RoamingCancelacionCostoController : Controller
+    {
+        ICPruebaEntities db = new ICPruebaEntities();
+        
+        FuncionesGeneralesController FNCGrales = new FuncionesGeneralesController();
+        // GET: RoamingCancelacionCosto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CargarCSV(HttpPostedFileBase archivoCSV, int lineaNegocio)
+        {
+            object respuesta = null;
+            
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
