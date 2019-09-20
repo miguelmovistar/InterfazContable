c1fc5bb65f7c4dd262e4c66f31c7d192a5b3cff3
Gerencia Virtual <gerencia.virtual@hotmail.com>
2019-09-20 16:22:18 -0500

    Se crea el controller para documento ingreso controller

---
+++ IC2/Controllers/RoamingDocumentoIngresoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IC2.Controllers
+{
+    public class RoamingDocumentoIngresoController : Controller
+    {
+        // GET: RoamingDocumentoIngreso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
